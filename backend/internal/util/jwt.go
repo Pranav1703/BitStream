a47ab5880cd6456355a797ad13590a2d0cfad5c8
Pranav1703 <pranavsai2005@gmail.com>
2025-02-24 11:35:15 +0530

    minor changes to auth code

--- backend/internal/util/jwt.go
+++ backend/internal/util/jwt.go
@@ -15,7 +15,7 @@ func CreateToken(username string)(string,error){
 
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256,jwt.MapClaims{
 		"username": username,
-		"exp": time.Now().Add(2 *time.Hour).Unix(),
+		"exp": time.Now().Add(4 *time.Hour).Unix(),
 		"iat": time.Now().Unix(),
 	})
 
@@ -37,12 +37,12 @@ func VerifyToken(r *http.Request) (string, error) {
 	// Parse and validate the token
 	tokenString := cookie.Value
 	claims := jwt.MapClaims{}
-	_, err = jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		return SecretKey, nil
 	})
 
-	if err != nil {
-		return "", fmt.Errorf("invalid token")
+	if err != nil || !token.Valid {
+		return "", fmt.Errorf("invalid or expired token")
 	}
 
 	// iat,_ := claims["iat"].(float64);
@@ -50,6 +50,11 @@ func VerifyToken(r *http.Request) (string, error) {
 	// fmt.Println("issued at: ",time.Unix(int64(iat),0))
 	// fmt.Println("expiry : ",time.Unix(int64(exp),0))
 
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", fmt.Errorf("invalid token data")
+	}
+	
 	username, ok := claims["username"].(string)
 	if !ok {
 		return "", fmt.Errorf("invalid token data")
