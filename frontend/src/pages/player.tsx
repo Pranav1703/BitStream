3a6aeb13456855dd2c247e26df849654d3646ff9
Pranav1703 <pranavsai2005@gmail.com>
2025-04-02 01:02:32 +0530

    added new player

--- frontend/src/pages/player.tsx
+++ frontend/src/pages/player.tsx
@@ -1,31 +1,34 @@
-  import { useEffect, useState } from "react";
-  import ReactPlayer from "react-player";
-  import { useSearchParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import ReactPlayer from "react-player";
+import { useSearchParams } from "react-router-dom";
+import shaka from "shaka-player";
 
-  const Player = () => {
+const Player = () => {
 
-    const [streamURL, setStreamURL] = useState("");
-    const [searchParams] = useSearchParams();
-    const magnet = searchParams.get("magnet");
+  const [streamURL, setStreamURL] = useState("");
+  const [searchParams] = useSearchParams();
+  const magnet = searchParams.get("magnet");
+  const videoRef = useRef(null);
 
-    useEffect(() => {
-      if (magnet) {
-        const encodedMagnet = encodeURIComponent(magnet);
-        setStreamURL(`${import.meta.env.VITE_SERVER}/stream?magnet=${encodedMagnet}`);
-      }
-      console.log("magnet link -> ",magnet)
-    }, [magnet]);
-
-    return (
-      <div>
-      
-        {streamURL && (
-          <div style={{ marginTop: "20px" }}>
-            <ReactPlayer url={streamURL} controls width="100%" height="500px" />
-          </div>
-        )}
-      </div>
-    );
-  };
+  useEffect(() => {
+    if (magnet) {
+      const encodedMagnet = encodeURIComponent(magnet);
+      setStreamURL(`${import.meta.env.VITE_SERVER}/stream?magnet=${encodedMagnet}`);
+      const player = new shaka.Player(videoRef.current)
+    }
+    console.log("magnet link -> ",magnet)
+  }, [magnet]);
+  
+  return (
+    <div>
+    
+      {streamURL && (
+        <div style={{ marginTop: "20px" }}>
+          <ReactPlayer url={streamURL} controls width="100%" height="500px" />
+        </div>
+      )}
+    </div>
+  );
+};
 
   export default Player;
