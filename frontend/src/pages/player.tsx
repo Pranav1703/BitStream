e112f0050d899c978920084daa41afe63e78062e
Pranav1703 <pranavsai2005@gmail.com>
2025-04-19 20:04:29 +0530

    .

--- frontend/src/pages/player.tsx
+++ frontend/src/pages/player.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import { useSearchParams } from "react-router-dom";
 
